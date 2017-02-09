cfd73ad887cd7feb79a7466f8290e28b17c98848
Alejandro Rodríguez Natal <rodrigueznatalandy@gmail.com>
2017-02-08 21:59:26 -0500

    Fixed the carousel

--- app/shared/research.js
+++ app/shared/research.js
@@ -2,31 +2,6 @@
 var Research = (function () {
     function Research() {
     }
-    function setupDB(){
-	var sql = require('sql');
-
-	sql.setDialect('postgres');
-
-	/*
-		Declares the Schematic to be utlized by the table
-	*/
-	var paper = sql.define(
-		{
-			name:'paper',
-			columns:['date','contributor','link', 'chemicalName']
-		}
-	);
-
-	/*
-		Updates The Table with provided fields
-	*/
-	var post = sql.define(
-		{
-			name:'post',
-			columns:['date','contributor','link','chemicalName']			
-		}
-	);
-    }
     return Research;
 }());
 exports.Research = Research;
